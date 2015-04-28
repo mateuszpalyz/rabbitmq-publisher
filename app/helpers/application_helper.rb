5b08f52ba84d175c58eb20e1f7030fc1377bb14d
Mateusz Palyz <m.palyz@pilot.co>
2015-04-28 21:56:09 +0200

    #11 use helper to display correct icon in currencies index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def boolean_icon(value)
+    value ? '<i class="glyphicon glyphicon-ok"></i>'.html_safe : '<i class="glyphicon glyphicon-remove"></i>'.html_safe
+  end
 end
