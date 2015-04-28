5947661f5bb3308667b8be8301f8b5a2b980d011
Mateusz Palyz <m.palyz@pilot.co>
2015-04-28 21:28:28 +0200

    #7 use update_attributes in ack worker

--- app/workers/ack_worker.rb
+++ app/workers/ack_worker.rb
@@ -7,7 +7,7 @@ class AckWorker
     currency = currency_with_uuid(message['uuid'])
 
     if currency
-      currency.update_attribute("ack#{message['id']}", true)
+      currency.update_attributes("ack#{message['id']}" => true)
       ack!
     else
       reject!
